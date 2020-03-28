2b0c9fbc60e73471e5358306f578fc0c724ce775
Trevor Broaddus <trevor.broaddus@gmail.com>
2020-03-27 23:54:19 -0500

    WIP hardcode data to reduce api calls; got restaurant info on the page.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 import TripAdvisorService from './service/trip_advisor_service';
 
+import Restaurant from './components/Restaurant';
+
 class App extends Component {
-  async render() {
+  constructor(props) {
+    super(props);
+
     const service = new TripAdvisorService();
-    const response = await service.findRestaurants("35.7345", "-86.9117", "35.8589", "-86.8698");
-    console.log(response.body);
+
+    this.state = {
+      restaurants: service.findRestaurants("35.7345", "-86.9117", "35.8589", "-86.8698")
+    };
+  }
+
+  render() {
     return (
       <div className="App">
-
+          {this.state.restaurants.data.map((restaurant, idx) => {
+              return (<Restaurant key={idx} restaurant={restaurant}/>);
+          })}
       </div>
     );
   }
---
+++ src/components/Restaurant.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Restaurant extends Component {
+    constructor(props) {
+        super(props);
+        const { restaurant } = props;
+
+        let image_url = "";
+        if (restaurant.photo) {
+            if (restaurant.photo.images) {
+                image_url = restaurant.photo.images.medium.url;
+            }
+        }
+
+        this.state = {
+            name: restaurant.name,
+            image_url: image_url,
+            website_url: restaurant.website,
+            phone: restaurant.phone
+        };
+    }
+
+    render() {
+        return (
+            <div className="Restaurant">
+                <div className="Restaurant-image">
+                    <a href={this.state.website_url}>
+                        <img src={this.state.image_url} />
+                    </a>
+                </div>
+
+                <div className="Restaurant-name">{this.state.name}</div>
+                <div className="Restaurant-phone">{this.state.phone}</div>
+            </div>
+        );
+    }
+}
